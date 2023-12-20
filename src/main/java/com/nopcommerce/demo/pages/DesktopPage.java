95d76a1a6f5c03b1205ca7432cc0050028343c22
darshil280996 <khuntdarshil28@gmail.com>
2023-12-19 19:50:00 -0500

    nopcommerce-sw6

---
+++ src/main/java/com/nopcommerce/demo/pages/DesktopPage.java
@@ -0,0 +1,38 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.browserfactory.ManageBrowser;
+import com.nopcommerce.demo.utilities.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class DesktopPage extends Utility {
+
+    private static final Logger log = LogManager.getLogger(ManageBrowser.class);
+
+    /******************************** All Elements' Path on Desktops page ***********************************************/
+
+    @CacheLookup
+    @FindBy(xpath = "//h1[normalize-space()='Desktops']")
+    WebElement welcomeText;
+
+    @CacheLookup
+    @FindBy(xpath = "//h2[@class='product-title']//a[normalize-space()='Build your own computer']")
+    WebElement buildYourOwnComputerLink;
+
+
+    /******************************** All Elements' Action Methods on Desktops page *************************************/
+
+    public String getWelcomeText() {
+        log.info("Getting Computers Text : " + welcomeText.toString());
+        return getTextFromElement(welcomeText);
+    }
+
+    public void clickOnBuildYourOwnComputerLink () {
+        clickOnElement(buildYourOwnComputerLink);
+        log.info("Clicking On Build Your Computer link : " + buildYourOwnComputerLink.toString());
+    }
+}
+
